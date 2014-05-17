89f3ba4f2700a6bbefdd18daf243018f87ea539f
Jason Sackey <planetearthjason@gmail.com>
2014-05-17 18:09:01 +0100

    JS module with private/public members

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,33 +1,28 @@
+// HELLO COSMIC FRIENDS
+// The module pattern is sweet
+// http://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript
+
+var APP;
+
 (function ($) {
   "use strict";
-  var app = {
-    shapes: function() {
-    
-    
-    },
-    initSpotlight: function() {
-      // (depends on data-hrefs plugin)
-      $('.spotlight').each(function(i, e) {
-        $(this).attr('data-href', $(this).find('a').first().attr('href'));
+  APP = (function() {
+    function drawColourPalette() {
+      $('.palette li').each(function() {
+        var colourValue = $(this).text();
+        $(this).prepend('<span class="palette-sample" style="background:'
+                + colourValue + '"></span>');
       });
-    },
-    drawColourPalette: function() {
-      $('.palette li').each(function(i, e) {
-        var colourValue = $(e).text();
-        $(e).prepend('<span class="palette-sample" style="background:' + colourValue + 
-        '"></span>')
-      });
-    },
-    init: function () {
-      app.initSpotlight();
-      app.drawColourPalette();
-      // initialises data-hrefs plugin
-      sethrefs();
-      app.shapes();
     }
-  }
-  app.init();
-})(jQuery);
 
-// Module pattern FTW: 
-// http://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript
+    return {
+      init: function() {
+        drawColourPalette();
+      }
+    };
+  }());
+    
+  $(function () {
+    APP.init();
+  });
+}(jQuery));
